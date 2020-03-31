01a38b20bdefd3df29f594b270d8e42d69d4ef8f
nino <nino_roldan@yahoo.com>
2020-03-31 21:21:19 +0900

    created test.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email = 'test@email.com',
+            password ='secret'
+        )
